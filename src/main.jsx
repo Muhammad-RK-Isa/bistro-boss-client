bb146ae18c0a7904faab62aeda6d603271271c2e
Muhammad Isa <muhammad-rk-isa@outlook.com>
2023-06-08 10:25:58 +0600

    new files added

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import { RouterProvider } from 'react-router-dom';
-import Routes from './Routes/Routes';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import {
+  RouterProvider,
+} from "react-router-dom";
+import { router } from './Routes/Routes';
+import { HelmetProvider } from 'react-helmet-async';
+import AuthProvider from './providers/AuthProvider';
 
-ReactDOM.createRoot( document.getElementById( 'root' ) ).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={ Routes } />
+    <AuthProvider>
+      <HelmetProvider>
+        <div className='max-w-screen-xl mx-auto'>
+          <RouterProvider router={router} />
+        </div>
+      </HelmetProvider>
+    </AuthProvider>
   </React.StrictMode>,
-);
+)
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./src/**/*.{js,jsx}"
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 }
 
